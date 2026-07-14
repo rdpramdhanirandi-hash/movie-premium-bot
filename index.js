c333c6c6ff43f13222d91100f8fd70affaa78a6e
rdpramdhanirandi-hash <rdpramdhanirandi@gmail.com>
2026-07-15 00:27:52 +0700

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-const TelegramBot=require('node-telegram-bot-api');
-const token=process.env.BOT_TOKEN;
-const bot=new TelegramBot(token,{polling:true});
-bot.onText(/\/start/,m=>bot.sendMessage(m.chat.id,"👋 Selamat datang di Movie Premium!"));
-bot.on("message",m=>{
- const t=(m.text||"").toLowerCase();
- if(t==="halo") bot.sendMessage(m.chat.id,"Halo juga! 👋");
- if(t==="menu") bot.sendMessage(m.chat.id,"📋 Menu\n/start\nmenu\nhalo");
-});
